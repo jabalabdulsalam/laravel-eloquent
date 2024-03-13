332b76bffdaa46bc7cb8fd3d4abd6c8e07cfa6cb
Jabal Abdul Salam <jabalabdulsalam@gmail.com>
2024-03-13 12:59:55 +0700

    sudah selesai materi model, insert, query builder dan insert many

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE belajar_laravel_eloquent;
+
+USE belajar_laravel_eloquent;
+
+SHOW TABLES;
+
+DESC categories;
+
+SELECT * FROM categories;
+
---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function testInsert()
+    {
+        $category = new Category();
+        $category->id = "GADGET";
+        $category->name = "Gadget";
+        $result = $category->save();
+
+        self::assertTrue($result);
+    }
+
+    public function testInsertMany()
+    {
+        $categories = [];
+        for ($i = 0; $i < 10; $i++){
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Name $i"
+            ];
+        }
+
+        //$result = Category::query()->insert($categories);
+        $result = Category::insert($categories);
+
+        self::assertTrue($result);
+
+        //$total = Category::query()->count();
+        $total = Category::count();
+        self::assertEquals(10, $total);
+    }
+}

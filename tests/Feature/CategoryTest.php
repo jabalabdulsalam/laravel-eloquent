70a90487c95687ea92371b0a5c2dfe7c804ede14
Jabal Abdul Salam <jabalabdulsalam@gmail.com>
2024-03-13 20:54:56 +0700

    telah menyelesaikan materi delete

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertTrue;
 
 class CategoryTest extends TestCase
 {
@@ -22,7 +25,7 @@ class CategoryTest extends TestCase
     public function testInsertMany()
     {
         $categories = [];
-        for ($i = 0; $i < 10; $i++){
+        for ($i = 0; $i < 10; $i++) {
             $categories[] = [
                 "id" => "ID $i",
                 "name" => "Name $i"
@@ -37,5 +40,83 @@ class CategoryTest extends TestCase
         //$total = Category::query()->count();
         $total = Category::count();
         self::assertEquals(10, $total);
+    }
+
+    public function testFind()
+    {
+        $this->seed(CategorySeeder::class);
+
+        // $category = Category::query()->find("FOOD");
+        $category = Category::find("FOOD");
+        self::assertNotNull($category);
+        self::assertEquals("FOOD", $category->id);
+        self::assertEquals("Food", $category->name);
+        self::assertEquals("Food Category", $category->description);
+    }
+
+    public function testUpdate()
+    {
+        $this->seed(CategorySeeder::class);
+
+        //$category = Category::query()->find("FOOD");
+        $category = Category::find("FOOD");
+        $category->name = "Food Updated";
+
+        $result = $category->update();
+
+        self::assertTrue($result);
+    }
+
+    public function testSelect()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $category = new Category();
+            $category->id = "$i";
+            $category->name = "Category $i";
+            $category->save();
+        }
+
+        $categories = Category::query()->whereNull("description")->get();
+        self::assertEquals(5, $categories->count());
+        $categories->each(function ($category) {
+            self::assertNull($category->description);
+
+            $category->description = "Updated";
+            $category->update();
+        });
+    }
+
+    public function testUpdateMany()
+    {
+        $categories = [];
+        for ($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Name $i"
+            ];
+        }
+
+            $result = Category::insert($categories);
+            self::assertTrue($result);
+
+            Category::whereNull("description")->update([
+                "description" => "Updated"
+            ]);
+
+            $total = Category::where("description", "=", "Updated")->count();
+            self::assertEquals(10, $total);
+        }
+
+    public function testDelete()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::query()->find("FOOD");
+        $result = $category->delete();
+
+        self::assertTrue($result);
+
+        $total = Category::query()->count();
+        assertEquals(0, $total);
     }
 }
